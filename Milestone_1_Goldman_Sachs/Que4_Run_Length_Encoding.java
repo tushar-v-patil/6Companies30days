d0c6e9080bb7779deb10f7ad0140a9d9706d3334
Tushar Patil <68781168+tushar-v-patil@users.noreply.github.com>
2022-01-01 20:10:31 +0530

    Update Que4_Run_Length_Encoding.java

--- Milestone_1_Goldman_Sachs/Que4_Run_Length_Encoding.java
+++ Milestone_1_Goldman_Sachs/Que4_Run_Length_Encoding.java
@@ -3,27 +3,29 @@
 Que-4 https://practice.geeksforgeeks.org/problems/run-length-encoding/1/
 
 */
-
+class Solution{
 	String encode(String str)
 	{
-          StringBuilder sb = new StringBuilder();
-          int n = str.length();
-          int count = 0;
-          char curr = '#';
-          for(int i = 0; i < n; i++){
-              if(i == 0){
-                  curr = str.charAt(i);
-                  count++;
-              }
-              else if(str.charAt(i) == str.charAt(i-1)){
-                  count++;
-              }
-              else if(str.charAt(i) != str.charAt(i-1)){
-                  sb.append(curr + "" + count);
-                  curr = str.charAt(i);
-                  count = 1;
-              }
-          }
-          sb.append(curr + "" + count);
-          return sb.toString();
+	  StringBuilder sb = new StringBuilder();
+	  int n = str.length();
+	  int count = 0;
+	  char curr = '#';
+	  for(int i = 0; i < n; i++){
+	      if(i == 0){
+		  curr = str.charAt(i);
+		  count++;
+	      }
+	      else if(str.charAt(i) == str.charAt(i-1)){
+		  count++;
+	      }
+	      else if(str.charAt(i) != str.charAt(i-1)){
+		  sb.append(curr + "" + count);
+		  curr = str.charAt(i);
+		  count = 1;
+	      }
+	  }
+	  sb.append(curr + "" + count);
+	  return sb.toString();
 	}
+
+}
