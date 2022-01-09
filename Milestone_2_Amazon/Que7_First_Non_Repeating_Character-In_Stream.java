0f9266bf8b6841002cb88fcab52bee0ce2e0a37b
Tushar Patil <68781168+tushar-v-patil@users.noreply.github.com>
2022-01-09 19:03:54 +0530

    Update Que7_First_Non_Repeating_Character-In_Stream.java

--- Milestone_2_Amazon/Que7_First_Non_Repeating_Character-In_Stream.java
+++ Milestone_2_Amazon/Que7_First_Non_Repeating_Character-In_Stream.java
@@ -1,6 +1,8 @@
 /*
 Que7: https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1#
 */
+
+// Solution 1: In java use linked hashmap
 class Solution
 {
     public String FirstNonRepeating(String s)
@@ -19,6 +21,36 @@ class Solution
                 }
             }
             sb.append(currChar+"");
+        }
+        return sb.toString();
+    }
+}
+
+// Solution 2 : Using sliding window
+class Solution
+{
+    public String FirstNonRepeating(String s)
+    {
+        int n = s.length();
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] hash = new int[26];
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            
+            if(hash[ch-'a'] == 0){
+                dq.addLast(ch-'a');
+            }
+            
+            hash[ch-'a']++;
+            
+            while(!dq.isEmpty() && hash[dq.peekFirst()] != 1){
+                dq.pollFirst();
+            }
+            
+            if(dq.isEmpty()) sb.append("#");
+            else sb.append((char)('a' + dq.peekFirst()));
         }
         return sb.toString();
     }
