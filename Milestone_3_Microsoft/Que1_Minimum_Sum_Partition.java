d6a544a9f8d2f766ad1e774e5b258bc35be6c725
Tushar Patil <68781168+tushar-v-patil@users.noreply.github.com>
2022-01-11 17:57:57 +0530

    Create Que1_Minimum_Sum_Partition.java

---
+++ Milestone_3_Microsoft/Que1_Minimum_Sum_Partition.java
@@ -0,0 +1,38 @@
+/*
+Que1: https://practice.geeksforgeeks.org/problems/minimum-sum-partition3317/1/#
+*/
+class Solution
+{
+
+	public int minDifference(int arr[], int n) 
+	{ 
+	    int sum = 0;
+	    for(int e : arr) sum += e;
+	    return minDiff(arr,n,0,sum,0);
+	}
+	
+	Map<String,Integer> map = new HashMap<>();
+	public int minDiff(int[] arr, int n, int index, int sum, int currSum){
+	    String hash = index + "|" + currSum;
+	    
+	    if(index == n){
+	        int min = Math.abs((sum-currSum)-currSum);    
+	        return min;
+	    }
+	    
+	    if(map.containsKey(hash)){
+	        return map.get(hash);
+	    }
+	    
+	    // pick
+	    int op1 = minDiff(arr,n,index+1,sum,currSum+arr[index]);
+	    // not pick
+	    int op2 = minDiff(arr,n,index+1,sum,currSum);
+	    
+	    int min = Math.min(op1,op2);
+	    map.put(hash,min);
+	    
+	    return min;
+	}
+    
+}
